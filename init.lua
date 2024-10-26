d627fa12d3be018621abc91e0195509bca432b05
Riari <3583774+Riari@users.noreply.github.com>
2024-10-26 18:05:25 +0100

    Remove old vim.cmd.hi calls and update completion mappings

--- init.lua
+++ init.lua
@@ -786,9 +786,9 @@ require('lazy').setup({
         -- No, but seriously. Please read `:help ins-completion`, it is really good!
         mapping = cmp.mapping.preset.insert {
           -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-d>'] = cmp.mapping.select_next_item(),
           -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-s>'] = cmp.mapping.select_prev_item(),
 
           -- Scroll the documentation window [b]ack / [f]orward
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -797,11 +797,11 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          -- ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<C-a>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
-          ['<CR>'] = cmp.mapping.confirm { select = true },
+          --['<CR>'] = cmp.mapping.confirm { select = true },
           --['<Tab>'] = cmp.mapping.select_next_item(),
           --['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
@@ -863,10 +863,6 @@ require('lazy').setup({
       vim.api.nvim_set_hl(0, 'WinSeparator', { fg = '#424864' })
       vim.api.nvim_set_hl(0, 'WinBar', { bg = 'none' })
       vim.api.nvim_set_hl(0, 'WinBarNC', { bg = 'none' })
-      --vim.cmd.hi 'WinSeparator guifg=#424864'
-      --vim.cmd.hi 'WinBar guibg=none'
-      --vim.cmd.hi 'WinBarNC guibg=none'
-      --vim.cmd.hi 'DapUINormal guibg=none'
     end,
   },
 
